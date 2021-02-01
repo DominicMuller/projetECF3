2262726353eef18e6650c8a3c46df393a6196190
Domi Muller <domi@gmail.com>
2021-02-01 16:28:03 +0100

    C1.3 - I1 - Modif application

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -3,6 +3,8 @@ package appli;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("main pour les tests");
+		
+		System.out.println("modif I1 pour C1.3 ");
 	}
 
 }
